ae6c55ca18e48859a1d02cf4183ccf5e10889587
kadirzkaya <bmkozkaya@gmail.com>
2022-09-26 00:58:38 +0300

    2X3'lük matrisin transpozunu bulma

--- src/MatrisTranspozu.java
+++ src/MatrisTranspozu.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class MatrisTranspozu {
+import java.util.Scanner;
+
+public class MatrisTranspozu {
+    public static void main(String[] args) {
+        String[][] arr=new String[2][3];
+        String[][] newArr=new String[3][2];
+        String t;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Matris : ");
+        for (int i=0; i<2;i++){
+            t=sc.nextLine();
+
+            for (int j=0;j<t.split(" ").length;j++){
+                arr[i][j]=t.split(" ")[j];
+            }
+
+        }
+
+        for (int i=0; i<3;i++){
+
+            for (int j=0;j<2;j++){
+                newArr[i][j]=arr[j][i];
+            }
+
+        }
+
+        System.out.println("Transpoze : ");
+        for (String[] k:newArr){
+            for (String m:k){
+                System.out.print(m+" ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+    }
 }
